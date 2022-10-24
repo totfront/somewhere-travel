87d8e89e44c0aa7849ff3582b3b238f7c6b00c97
Roman Kosh <110398466+LalightKosh@users.noreply.github.com>
2022-10-24 20:43:39 +0400

    fix: spelling
    deleted a whitespace

--- src/components/Lead/Lead.js
+++ src/components/Lead/Lead.js
@@ -6,7 +6,7 @@ function Lead() {
     <section className={styles.wrapper}>
       <title className={styles.title}>Путешествия по России</title>
       <subtitle className={styles.subtitle}>
-        Настоящая страна не в выпусках новостей, а здесь.
+        Настоящая странане в выпусках новостей, а здесь.
       </subtitle>
       <figure className={styles.figure}>
         <figcaption className={styles.caption}>ваша полка — верхняя</figcaption>
