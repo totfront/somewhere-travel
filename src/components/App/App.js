dc8c63c0a6e17071982bff7189beae228783f327
Roman Kosh <110398466+LalightKosh@users.noreply.github.com>
2022-11-20 22:58:07 +0200

    Feat: Intro for 320px screens

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import styles from "./App.module.css";
 import Header from "../Header/Header";
 import Lead from "../Lead/Lead";
+import Intro from "../Intro/Intro";
 
 function App() {
   return (
     <div className={styles.app}>
       <Header />
       <Lead />
+      <Intro />
     </div>
   );
 }
---
+++ src/components/Intro/Intro.js
@@ -0,0 +1,45 @@
+import styles from "./Intro.module.css";
+
+function Intro() {
+  return (
+    <section className={styles.wrapper}>
+      <title className={styles.title}>Чего мы там не видели?</title>
+      <p className={styles.Introtext}>
+        По опросам ВЦИОМ, 95% россиян мечтают куда-нибудь поехать, но только 36%
+        планируют провести отпуск в родной стране. Мол, чего мы тут, дома, не
+        видели? На самом деле, Россия — это целая вселенная с ласковым морем
+        юга, густыми лесами Саян и суровыми льдами плато Путорана. А ещё увидеть
+        все эти красоты можно без миллионов на счету, загранпаспорта и
+        многочасовых перелетов. Как, например, Вера Башмакова — смелая молодая
+        мама, которая взяла в охапку троих детей, усадила их в свою «Ладу» и
+        проехала 20 тысяч километров по родной стране. Мы выбрали и описали
+        некоторые интересные места, достойные вашего отпуска.
+      </p>
+      <li className={styles.Introlist}>
+        <ul className={styles.Introitem}>
+          <span className={styles.bolditem}>Часовых поясов</span> 11
+        </ul>
+        <ul className={styles.Introitem}>
+          <span className={styles.bolditem}>
+            Объектов природного наследия ЮНЕСКО
+          </span>{" "}
+          12
+        </ul>
+        <ul className={styles.Introitem}>
+          <span className={styles.bolditem}>
+            Объектов культурного наследия ЮНЕСКО
+          </span>{" "}
+          16
+        </ul>
+        <ul className={styles.Introitem}>
+          <span className={styles.bolditem}>Природных заповедников</span> 105
+        </ul>
+        <ul className={styles.Introitem}>
+          <span className={styles.bolditem}>Аэропортов</span> 241
+        </ul>
+      </li>
+    </section>
+  );
+}
+
+export default Intro;
