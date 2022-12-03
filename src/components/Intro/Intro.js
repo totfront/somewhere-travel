9c1f87da9d8abb1ec4d76f14c04ef2aed621175f
Roman Kosh <110398466+LalightKosh@users.noreply.github.com>
2022-12-03 14:30:53 +0200

    Fix: changed the classnames to conventional ones

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -4,38 +4,38 @@ function Intro() {
   return (
     <section className={styles.wrapper}>
       <title className={styles.title}>Чего мы там не видели?</title>
-      <p className={styles.Introtext}>
+      <p className={styles.introText}>
         По опросам ВЦИОМ, 95% россиян мечтают куда-нибудь поехать, но только 36%
         планируют провести отпуск в родной стране. Мол, чего мы тут, дома, не
         видели? На самом деле, Россия — это целая вселенная с ласковым морем
-        юга, густыми лесами Саян и суровыми льдами плато Путорана. А ещё увидеть
-        все эти красоты можно без миллионов на счету, загранпаспорта и
+        юга, густыми лесами Саян и суровыми льдами плато Путорана. А ещё
+        увидеulть все эти красоты можно без миллионов на счету, загранпаспорта и
         многочасовых перелетов. Как, например, Вера Башмакова — смелая молодая
         мама, которая взяла в охапку троих детей, усадила их в свою «Ладу» и
         проехала 20 тысяч километров по родной стране. Мы выбрали и описали
         некоторые интересные места, достойные вашего отпуска.
       </p>
-      <ul className={styles.Introlist}>
-        <li className={styles.Introitem}>
-          <span className={styles.bolditem}>Часовых поясов</span> 11
+      <ul className={styles.introList}>
+        <li className={styles.introItem}>
+          <span className={styles.boldItem}>Часовых поясов</span> 11
         </li>
-        <li className={styles.Introitem}>
-          <span className={styles.bolditem}>
+        <li className={styles.introItem}>
+          <span className={styles.boldItem}>
             Объектов природного наследия ЮНЕСКО
           </span>{" "}
           12
         </li>
-        <li className={styles.Introitem}>
-          <span className={styles.bolditem}>
+        <li className={styles.introItem}>
+          <span className={styles.boldItem}>
             Объектов культурного наследия ЮНЕСКО
           </span>{" "}
           16
         </li>
-        <li className={styles.Introitem}>
-          <span className={styles.bolditem}>Природных заповедников</span> 105
+        <li className={styles.introItem}>
+          <span className={styles.boldItem}>Природных заповедников</span> 105
         </li>
-        <li className={styles.Introitem}>
-          <span className={styles.bolditem}>Аэропортов</span> 241
+        <li className={styles.introItem}>
+          <span className={styles.boldItem}>Аэропортов</span> 241
         </li>
       </ul>
     </section>
