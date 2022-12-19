f73ebd64720ec1bd7d25a178df368dfedc15c147
Roman Kosh <110398466+LalightKosh@users.noreply.github.com>
2022-12-19 19:38:19 +0200

    chore: started working on changing languages buttons

--- src/components/Lead/Lead.js
+++ src/components/Lead/Lead.js
@@ -1,10 +1,11 @@
 import styles from "./Lead.module.css";
 import route from "../../Images/route.svg";
+import texts from "../../shared/texts.json";
 
 function Lead() {
   return (
     <section className={styles.wrapper}>
-      <h1 className={styles.title}>Путешествия по России</h1>
+      <h1 className={styles.title}>{texts.Lead.title.ru}</h1>
       <h2 className={styles.subtitle}>
         Настоящая страна не в выпусках новостей, а здесь.
       </h2>
