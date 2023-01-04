44b572042129015627a061f2c3528fcd183db558
Roman Kosh <110398466+LalightKosh@users.noreply.github.com>
2023-01-04 20:41:55 +0200

    Feat: finished place

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,6 @@ import styles from "./App.module.css";
 import Header from "../Header/Header";
 import Lead from "../Lead/Lead";
 import Place from "../Place/Place";
-// import PhotoGrid from "../PhotoGrid/PhotoGrid";
 import Intro from "../Intro/Intro";
 
 function App() {
