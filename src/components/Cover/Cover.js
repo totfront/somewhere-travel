6b0de50dce5416ef4184bd60a29b0576fdc8f0b2
Roman Kosh <110398466+LalightKosh@users.noreply.github.com>
2023-01-11 20:04:55 +0200

    Feat: finished cover

--- src/components/Cover/Cover.js
+++ src/components/Cover/Cover.js
@@ -1,14 +1,22 @@
-import coverPhoto from "../../images/coverPhoto.png";
+import trainsPhoto from "../../images/coverPhoto.png";
 import styles from "./Cover.module.css";
 
 function Cover() {
   return (
     <section className={styles.wrapper}>
-      <h2 className={styles.title}>До Байкала «на собаках»</h2>
-      <h3 className={styles.subtitle}>
-        По мотивам учебной темы о Транссибе — путешествие от столицы до Байкала
-        на электричках.
-      </h3>
+      <img
+        src={trainsPhoto}
+        alt="Train station"
+        className={styles.coverPhoto}
+      ></img>
+      <div className={styles.text}>
+        <h2 className={styles.title}>До Байкала «на собаках»</h2>
+        <h3 className={styles.subtitle}>
+          По мотивам учебной темы о Транссибе — путешествие от столицы до
+          Байкала на электричках.
+        </h3>
+      </div>
+      <div className={styles.rectangle} />
     </section>
   );
 }
